c5ecdf704d7c47992a5ea8d83d9e82ad47210928
Phuoc Hoan Nguyen <100815132+hona97@users.noreply.github.com>
2022-12-03 21:03:56 +0700

    Add files via upload

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->string('userID')->primary('userID');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('image');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->timestamp('blocked_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
